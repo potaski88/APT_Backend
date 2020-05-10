afcba0e58e171ae78fbbade190ba8ba12159439f
ampritra <potaski88@gmail.com>
2020-05-10 18:42:31 +0200

    first

--- DB_config.js
+++ DB_config.js
@@ -317,6 +317,10 @@ async function updateProduct({id, price}) {
 
 
 async function test({item}) {
+
+    return item
+
+    
     const testConnect = {
         user: "ofbugdwpkfgivl",
         password: "cc6fe18e3014457b6b03884688458dcdc22199990d06b22924710e7213c1df82",
