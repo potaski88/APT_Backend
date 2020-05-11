4f1446215d7f2c8382b6767c04b151303bbcc811
ampritra <potaski88@gmail.com>
2020-05-11 18:56:40 +0200

    first

--- utils.js
+++ utils.js
@@ -5,11 +5,11 @@ const sendRegistrationMail = async (email, code) => {
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(process.env.SENDGRID_KEY);
     const msg = {
-      to: email,
+      to: "matwolmu@gmail.com",
       from: 'ampritra@gmail.com',
       subject: 'Amazon Price Tracker Registration',
       text: 'Your registration code: ' + code,
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      html: '',
     };
     sgMail.send(msg);
     
