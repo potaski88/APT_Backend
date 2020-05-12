75e63e6de80eb056be590a3501731a518f6e75b5
ampritra <potaski88@gmail.com>
2020-05-12 09:10:06 +0200

    first

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 
 const sendRegistrationMail = async () => {
     const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey("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");
-//    sgMail.setApiKey(process.env.SENDGRID_KEY);
+//    sgMail.setApiKey("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");
+    sgMail.setApiKey(process.env.SENDGRID_KEY);
     const msg = {
       to: "ampritra@gmail.com",
       from: 'ampritra@gmail.com',
