95610ad34a1a6b8717a5d10db968e6948b2fa258
ampritra <potaski88@gmail.com>
2020-05-12 09:37:40 +0200

    first

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ const sendRegistrationMail = async () => {
 //    sgMail.setApiKey("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");
     sgMail.setApiKey(process.env.SENDGRID_KEY);
     const msg = {
-      to: "ampritra@gmail.com",
+      to: "matwolmu@gmail.com",
       from: 'ampritra@gmail.com',
       subject: 'Amazon Price Tracker Registration',
       text: 'Your registration code: ',
