54e682422be800c174d927448738c527e6c3b1cc
ampritra <potaski88@gmail.com>
2020-06-26 19:15:26 +0200

    notificatrion

--- DB_config.js
+++ DB_config.js
@@ -313,11 +313,16 @@ async function deleteUser(userID) {
             .then(async x => {
                 return await client.query(`
                     SELECT id FROM public.products WHERE usr = ${userID};
+                    DELETE FROM public.products WHERE usr=${userID};
                 `)
             })
             .then(res => {
-                console.log("res.rows")
-                console.log(res.rows)
+                res.forEach(async item => {
+                    console.log("item.id: " + item.id)
+                    await client.query(`
+                        DROP TABLE public.product_${item.id};
+                    `)
+                })
                 return "OK"
             })
             .catch (error => console.log(error))
